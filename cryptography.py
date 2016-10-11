379a97ac7da21d7ec1bbbfdb19710ad56ed25481
WilsonRimberg <wilsonrimberg@hanovernorwichschools.org>
2016-10-11 13:48:03 -0400

    Updated from Brython Server: 10/11/2016 1:48:04 PM

--- cryptography.py
+++ cryptography.py
@@ -10,21 +10,30 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+associations1="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 command=str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
 while command not in ["e","d","q"]:
     print("Did not understand command, try again.")
     command=input("Enter e to encrypt, d to decrypt, or q to quit: ")
 if command== "q":
     print("Goodbye!")
-if command =="e":
-    message=list(input("Message: "))
-    key= list(input("Key: "))
-    added=[x+y for x,y in zip(message, key)]
+while command =="e":
+    message1=input("Message: ")
+    message=list(message1)
+    key1=input("Key: ")
+    key= list(key1)
     for x in range(0, len(message)):
         message[x]=associations.find(message[x])
     for x in range(0, len(key)):
         key[x]=associations.find(key[x])
     while len(key)<len(message):
         key.extend(key)
+    added=[x+y for x,y in zip(message, key)]
     for x in range(0, len(message)):
-        print(added[x], end=" ")
+        print(associations1[added[x]], end="")
+    command=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+while command=="d":
+    print("Message: "+associations1[added[x]])
+    print("Key: "+key1)
+    print(message1)
+    
