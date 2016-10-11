3343c5f379fbecfd46ae331c50a2aff7df3f6295
WilsonRimberg <wilsonrimberg@hanovernorwichschools.org>
2016-10-11 13:51:49 -0400

    Updated from Brython Server: 10/11/2016 1:51:49 PM

--- cryptography.py
+++ cryptography.py
@@ -30,10 +30,24 @@ while command =="e":
         key.extend(key)
     added=[x+y for x,y in zip(message, key)]
     for x in range(0, len(message)):
-        print(associations1[added[x]], end="")
+        bob=associations1[added[x]]
+        print(bob,end="")
     command=input("Enter e to encrypt, d to decrypt, or q to quit: ")
 while command=="d":
-    print("Message: "+associations1[added[x]])
+    message1=input("Message: ")
+    message=list(message1)
+    key1=input("Key: ")
+    key= list(key1)
+    for x in range(0, len(message)):
+        message[x]=associations.find(message[x])
+    for x in range(0, len(key)):
+        key[x]=associations.find(key[x])
+    while len(key)<len(message):
+        key.extend(key)
+    added=[x+y for x,y in zip(message, key)]
+    for x in range(0, len(message)):
+        bob=associations1[added[x]]
+    print("Message: "+bob)
     print("Key: "+key1)
     print(message1)
     command=input("Enter e to encrypt, d to decrypt, or q to quit: ")
