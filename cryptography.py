1e3d8834c31924cfdbd4fad60f798da0d3ed1ccc
WilsonRimberg <wilsonrimberg@hanovernorwichschools.org>
2016-10-14 13:25:48 -0400

    Updated from Brython Server: 10/14/2016 1:25:47 PM

--- cryptography.py
+++ cryptography.py
@@ -12,44 +12,41 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 associations1="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 command=str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
-while command not in ["e","d","q"]:
-    print("Did not understand command, try again.")
+while command  not in ["q"]:
+    if command not in ["e", "d"]:
+        print("Did not understand command, try again.")
+    if command =="e":
+        message1=input("Message: ")
+        message=list(message1)
+        key1=input("Key: ")
+        key= list(key1)
+        for x in range(0, len(message)):
+            message[x]=associations.find(message[x])
+        for x in range(0, len(key)):
+            key[x]=associations.find(key[x])
+        while len(key)<len(message):
+            key.extend(key)
+        added=[x+y for x,y in zip(message, key)]
+        for x in range(0, len(message)):
+            bob=associations1[added[x]]
+            print(bob,end="")
+        print( )
+    if command=="d":
+        message1=input("Message: ")
+        message=list(message1)
+        key1=input("Key: ")
+        key= list(key1)
+        for x in range(0, len(message)):
+            message[x]=associations.find(message[x])
+        for x in range(0, len(key)):
+            key[x]=associations.find(key[x])
+        while len(key)<len(message):
+            key.extend(key)
+        added=[x-y for x,y in zip(message, key)]
+        for x in range(0, len(message)):
+            bob=associations1[added[x]]
+            print(bob,end="")
+        print( )
     command=input("Enter e to encrypt, d to decrypt, or q to quit: ")
 if command== "q":
-    print("Goodbye!")
-while command =="e":
-    message1=input("Message: ")
-    message=list(message1)
-    key1=input("Key: ")
-    key= list(key1)
-    for x in range(0, len(message)):
-        message[x]=associations.find(message[x])
-    for x in range(0, len(key)):
-        key[x]=associations.find(key[x])
-    while len(key)<len(message):
-        key.extend(key)
-    added=[x+y for x,y in zip(message, key)]
-    for x in range(0, len(message)):
-        bob=associations1[added[x]]
-        print(bob,end="")
-    print( )
-    command=input("Enter e to encrypt, d to decrypt, or q to quit: ")
-while command=="d":
-    message1=input("Message: ")
-    message=list(message1)
-    key1=input("Key: ")
-    key= list(key1)
-    for x in range(0, len(message)):
-        message[x]=associations.find(message[x])
-    for x in range(0, len(key)):
-        key[x]=associations.find(key[x])
-    while len(key)<len(message):
-        key.extend(key)
-    added=[x-y for x,y in zip(message, key)]
-    for x in range(0, len(message)):
-        bob=associations1[added[x]]
-        print(bob,end="")
-    print( )
-    command=input("Enter e to encrypt, d to decrypt, or q to quit: ")
-    
-    
+        print("Goodbye!")
